7809d397a6285241f0ff71b2568bae6facfd112c
AKPM007 <akshaymonu2006@gmail.com>
2025-01-10 22:58:11 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -1,14 +1,14 @@
 import os
 
 OWNER_ID = 6354431214
-GROUP_ID = ""
-TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = "-4700638898"
+TOKEN = os.getenv("7828527701:AAHyELmGZhrnG8esnyj90hgnyYfNIOBEBIA")
 api_id = 22661264
 api_hash = "565265a8dc6d806f81133e17751bafdc"
 PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
 SUPPORT_CHAT = "dragona_support"
 UPDATE_CHAT = "PickBotUpdatesHQ"
 BOT_USERNAME = "Okarun_Game_bot"
-CHARA_CHANNEL_ID = -1002235251549
-MONGO_URL = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
+CHARA_CHANNEL_ID = -1002187797459
+MONGO_URL = "mongodb+srv://xblade1980:p0cizP1upf34yogf@cluster0.jqcpu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "Character_catcher"
